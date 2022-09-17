77aac719e626bd7652d507fa3f334c480f94c189
Ëddy Calero <Caleroeddy99@gmail.com>
2022-09-17 14:11:45 -0600

    Agregando el patron de arquitectura MVC

---
+++ YellowPages/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YellowPages.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
